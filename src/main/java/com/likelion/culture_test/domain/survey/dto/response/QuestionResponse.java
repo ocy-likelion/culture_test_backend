7e984504ee46d53578d41a62767129b32cb7993e
backnback <garam8796@naver.com>
2025-07-03 15:19:29 +0900

    Chore: 오타 수정 및 DTO schema 수정

--- src/main/java/com/likelion/culture_test/domain/survey/dto/response/QuestionResponse.java
+++ src/main/java/com/likelion/culture_test/domain/survey/dto/response/QuestionResponse.java
@@ -7,17 +7,19 @@ import lombok.Builder;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Schema(description = "질문 응답")
+@Schema(description = "질문 정보")
 @Builder
 public record QuestionResponse(
 
     @Schema(description = "질문 ID", example = "23")
     Long questionId,
 
+    @Schema(description = "질문 내용", example = "협업은 누가 맡았든 필요한 사람이 먼저 나서는 유연성이 핵심이라고 본다.")
     String content,
 
     PropertyResponse property,
 
+    @Schema(description = "선택형 질문 여부", example = "false")
     boolean isSelective,
 
     List<ChoiceResponse> choices
