a471cd85272bc3b707dd4483f3f8652fadb81f37
Youngheun <heeun.deco@gmail.com>
2025-07-07 10:08:47 +0900

    Chore: application-secret.yml 파일명 수정

--- src/main/java/com/likelion/culture_test/domain/user/entity/User.java
+++ src/main/java/com/likelion/culture_test/domain/user/entity/User.java
@@ -1,4 +1,46 @@
 package com.likelion.culture_test.domain.user.entity;
 
-public class User {
+import com.likelion.culture_test.domain.user.enums.SsoProvider;
+import com.likelion.culture_test.domain.user.enums.UserRole;
+import com.likelion.culture_test.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="role")
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="sso_provider")
+    private SsoProvider ssoProvider;
+
+    @Column(name="social_id")
+    private String socialId;
+
+    @Column(name="nickname")
+    private String nickname;
+
+    @Column(name="profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "refresh_token", unique = true)
+    private String refreshToken;
+
+
+
+
+
 }
--- src/main/java/com/likelion/culture_test/domain/user/enums/SsoProvider.java
+++ src/main/java/com/likelion/culture_test/domain/user/enums/SsoProvider.java
@@ -1,4 +1,5 @@
 package com.likelion.culture_test.domain.user.enums;
 
 public enum SsoProvider {
+    KAKAO, GOOGLE
 }
