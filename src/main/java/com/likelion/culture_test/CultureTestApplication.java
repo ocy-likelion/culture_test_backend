b458a174a6267a63aa86a1c8525c74f7e59bcebd
backnback <garam8796@naver.com>
2025-07-08 10:20:44 +0900

    Fix: Swagger 운영 서버 설정 추가

--- src/main/java/com/likelion/culture_test/CultureTestApplication.java
+++ src/main/java/com/likelion/culture_test/CultureTestApplication.java
@@ -1,5 +1,9 @@
 package com.likelion.culture_test;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -7,7 +11,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Culture Test API",
+				description = "인사담당자 성향 테스트 API 문서",
+				version = "1.0.0",
+				contact = @Contact(
+						name = "Likelion",
+						email = "garam8796@gmail.com"
+				)
+		),
+		servers = {
+				@Server(url = "http://localhost:8090", description = "개발 서버"),
+				@Server(url = "https://api.heun0.site", description = "운영 서버")
+		}
+)
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
